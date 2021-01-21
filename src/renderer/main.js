504e30f7e37be81a12e964b59aac0023ef37a9ad
devp2 <devp2@szkf.local>
2021-01-21 14:27:48 +0800

    引入elementUI成功

--- src/renderer/main.js
+++ src/renderer/main.js
@@ -4,30 +4,26 @@ import axios from 'axios'
 import App from './App'
 import router from './router'
 import store from './store'
-import vuetify from '@/plugins/vuetify' // path to vuetify export
-
-
-
-// import ElementUI from 'element-ui';
-// import 'element-ui/lib/theme-chalk/index.css';
-
-
+import ElementUI from 'element-ui';
+import 'element-ui/lib/theme-chalk/index.css';
 // import fs from 'fs'
 // import path from 'path'
 
+process.env['ELECTRON_DISABLE_SECURITY_WARNINGS'] = 'true'
 if (!process.env.IS_WEB) Vue.use(require('vue-electron'))
 Vue.http = Vue.prototype.$http = axios
 Vue.config.productionTip = false
-
-process.env['ELECTRON_DISABLE_SECURITY_WARNINGS'] = 'true'
+Vue.use(ElementUI)
 /* eslint-disable no-new */
 new Vue({
   components: { App },
   router,
   store,
-  vuetify,
+  el:'#app',
+  // vuetify,
   template: '<App/>'
 }).$mount('#app')
+
 
 // let fileContents = fs.readFileSync(path.join(__static, '/someFile.txt'), 'utf8')
 
