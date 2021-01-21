36ba31559e7152af1bfae2c872b41f7c0af95468
devp2 <devp2@szkf.local>
2021-01-21 17:49:55 +0800

    布局初步构建

--- src/renderer/components/NavView/topNav.vue
+++ src/renderer/components/NavView/topNav.vue
@@ -1,10 +1,10 @@
 <template>
   <div class="topNavigation">
-    <el-menu class="el-menu-demo" mode="horizontal">
-      <el-menu-item index="1">处理中心</el-menu-item>
-      <el-menu-item index="2">我的工作台</el-menu-item>
-      <el-menu-item index="3">消息中心</el-menu-item>
-      <el-menu-item index="4">订单管理</el-menu-item>
+    <el-menu class="el-menu-demo" mode="horizontal" :default-active="activeIndex">
+      <el-menu-item index="1">视频处理</el-menu-item>
+      <el-menu-item index="2">流媒体处理</el-menu-item>
+      <el-menu-item index="3">音频处理</el-menu-item>
+      <el-menu-item index="4">图片处理</el-menu-item>
     </el-menu>
   </div>
 </template>
@@ -13,7 +13,7 @@
   export default {
     data () {
       return {
-
+          activeIndex: "1"
       }
     }
   }
